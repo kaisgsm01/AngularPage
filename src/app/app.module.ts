5cb6594e656cf61ed246eb72bf3f00e8eacc37c5
MohamedElhedi-BenYedder <mohamedelhedi.benyedder@supcom.tn>
2021-03-06 00:09:20 +0100

    message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
 import {RouterModule} from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TestComponent
-  ],
+  declarations: [],
   imports: [
     BrowserModule,
     RouterModule
